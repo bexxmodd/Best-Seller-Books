70fb88c8a2b8b3d9e2ec60a98f80ef9485cbd8cd
Bexx Modd <bexxmodd@seas.upenn.edu>
2020-05-27 19:54:48 -0400

    combined puthon classes into one file

--- data/RawDataCollector.py
+++ data/RawDataCollector.py
@@ -1,8 +1,13 @@
-import requests
-import time
 import pandas as pd
 import numpy as np
+import pandas as pd
+import requests
+import json
+import time
+import re
 
+from tqdm import tqdm
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 
@@ -22,7 +27,6 @@ class GoodReadsScraper():
     def __str__(self):
         return f"You just scrapped {len(self.isbns)} books from the Goodreads.com!"
 
-
     def _goodreads_scraping(self):
         """ This method extracts the number of pages,
             book's edition from accrued HTML texts.
@@ -35,7 +39,6 @@ class GoodReadsScraper():
             edition.append(getattr(s.find(itemprop="bookFormat"), 'text', None))
         return isbn_13, pages, edition
             
-
     def data_converter(self):
         # Convertes previously extracted list into dataframe.
         i, p, e = self._goodreads_scraping()
@@ -43,7 +46,6 @@ class GoodReadsScraper():
                             'pages': p,
                             'edition': e})
         return df
-
 
     def _genre_scraping(self):
         """ 
@@ -56,13 +58,11 @@ class GoodReadsScraper():
             g.append(s.find_all(class_='actionLinkLite bookPageGenreLink')[:3])
         return isbn_13, g
             
-
     def genre_converter(self):
         # Convertes previously extracted list into dataframe.
         i, g = self._genre_scraping()
         df = pd.DataFrame({'isbn13': i, 'genres': g})
         return df
-    
     
     def _meta_scraping(self):
         """ This method extracts the metadata of the book.
@@ -76,7 +76,6 @@ class GoodReadsScraper():
             title.append(getattr(s.find(class_="gr-h1 gr-h1--serif"), 'text', None))
         return isbn_13, author, title
 
-
     def meta_converter(self):
         # Convertes previously extracted list into dataframe.
         i, a, t = self._meta_scraping()
@@ -84,7 +83,6 @@ class GoodReadsScraper():
                             'author': a,
                             'title': t})
         return df
-
 
     def _pop_scraping(self):
         """
@@ -98,7 +96,6 @@ class GoodReadsScraper():
             count.append(getattr(s.find(itemprop="ratingCount"), 'text', None))
         return isbn_13, rating, count
 
-
     def pop_converter(self):
         # Convertes previously extracted list into dataframe.
         i, r, c = self._pop_scraping()
@@ -106,7 +103,6 @@ class GoodReadsScraper():
                             'rating': r,
                             'count': c})
         return df
-
 
     def _cover_scraper(self):
         """ This method grabs cover picture URL.
@@ -123,13 +119,86 @@ class GoodReadsScraper():
                 cover_url.append(np.NaN)
         return isbn_13, cover_url
 
-
     def cover_url_converter(self):
         # Convertes previously extracted list into dataframe.
         i, cu = self._cover_scraper()
         df = pd.DataFrame({'isbn13': i,
                             'cover_url': cu})
         return df
+
+
+class TimesExtractor:
+    """ The purpose of the module is to extract books data from the Times API.
+
+    Times has a limit on how many requests you can send, so we have to do
+    with time lapses and then combine all the collected data into one list.
+
+    """
+    # Default api-key which can be changed.
+    api_key = 'djDLXwAoSfreMrzYGE5iacl7GUifIRrV'
+    # Default lapse between calls
+    lapse = 3
+
+    def __init__(self, start_date, end_date, frq):
+        """ Please enter dates in 'yyyy-mm-dd' format.
+
+        For frq -> 'D'=day; 'W'=week; 'M'=month; 'Y'=year.
+        """
+        self.start_date = start_date
+        self.end_date = end_date
+        self.frq = frq
+        # Raise Error if entered value doesn't match the pattern.
+        if not re.search(r"^\d{4}-\d{2}-\d{2}$", self.start_date):
+            raise ValueError('please enter start_date in \'yyyy-mm-dd\' format')
+        elif not re.search(r"^\d{4}-\d{2}-\d{2}$", self.end_date):
+            raise ValueError('please enter end_date in \'yyyy-mm-dd\' format')
+        elif self.frq not in ('D', 'W', 'M', 'Y'):
+            raise ValueError('please enter frequency in correct format')
+        else:
+            pass
+
+    def __str__(self):
+        return f"The bestellers list is from {self.start_date} to {self.end_date}"
+
+    def _datesrange(self):
+        if self.start_date >= self.end_date:
+            return [self.end_date]
+        else:
+            dates = [d for d in pd.date_range(start=self.start_date,
+                                        end=self.end_date,
+                                        freq=self.frq).strftime('%Y-%m-%d')]
+            return dates
+
+    @classmethod
+    def key(cls, key):
+        cls.api_key = key
+
+    @classmethod
+    def seconds(cls, second):
+        cls.lapse = second
+
+    def make_call(self):
+        """ This function iterates through the dates and sends the request
+        to the Times API for the given set of dates.
+        
+        Next, it returns all the dictionaries as a combined list.
+        """
+        super_list = []
+        print(f'Due to API\'s limitation, there is a {self.lapse} second lapse between calls')
+        for date in tqdm(self._datesrange()):
+            """ Times API has a daily limit on calls
+            and a requirement for intervals between calls.
+            
+            Thus, we set up the lapses between calls.
+            """
+
+            time.sleep(self.lapse)
+            print(date)
+            res = requests.get(f"https://api.nytimes.com/svc/books/v3/lists/{date}/combined-print-and-e-book-fiction.json?",
+                                params = {'api-key': self.api_key}).json()
+            super_list.append(res['results']['books'])
+        return super_list
+
 
 
 if __name__ == "__main__":
